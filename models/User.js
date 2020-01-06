207bc39322e03ff37b37a04c675b60ea2f13ba72
Osiel <ocl.vargasanchez@gmail.com>
2020-01-05 23:52:35 -0600

    create user model

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose")
+const passportLocalMongoose = require("passport-local-mongoose")
+
+const UserSchema = new UserSchema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+
+    firstName: {
+        type: String,
+        minlength: 1,
+        maxlength: 15,
+        required: true
+    },
+
+    lastName: {
+        type: String,
+        minlength: 1,
+        maxlength: 20,
+        required: true
+    },
+
+    password: {
+        type: String,
+        minlength: 6,
+        required: true
+    },
+
+    posts: [
+        { type: Schema.Types.ObjectId, ref: "Post" }
+    ],
+
+    likedPosts: [
+        { type: Schema.Types.ObjectId, ref: "Post" }
+    ],
+
+    likedComments: [
+        { type: Schema.Types.ObjectId, ref: "Post" }
+    ],
+
+    friends: [
+        { type: Schema.Types.ObjectId, ref: "User" }
+    ],
+
+    friendRequests: [
+        { type: Schema.Types.ObjectId, ref: "User" }
+    ]
+})
+
+UserSchema.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model('User', UserSchema)
